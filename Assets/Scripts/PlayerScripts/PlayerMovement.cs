be8acefceeb7f06be89ed72390eecc5c03271c9d
Vaibhav69Singh <vaibhav9237@gmail.com>
2024-10-16 19:16:26 +0530

    PlayerMovement added

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body;
+    private Animator anim;
+    [SerializeField]private float speed;
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+        //flip the character 
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(1.5f,1.5f,1);
+        }else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1.5f,1.5f,1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
     }
+
 }
