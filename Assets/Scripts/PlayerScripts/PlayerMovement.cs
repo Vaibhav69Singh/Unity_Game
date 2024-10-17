e70347f0e47257e4041eb5bdbcf3606c5b3950b4
Vaibhav69Singh <vaibhav9237@gmail.com>
2024-10-17 15:14:03 +0530

    Added new ainmation
    Added idle , running , jumping animation to the code

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     [SerializeField]private float speed;
+    private bool grounded;
     private void Awake()
     {
+        //Grab refrences
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -27,10 +30,24 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1.5f,1.5f,1);
         }
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            body.velocity = new Vector2(body.velocity.x, speed);
-        }
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //set animation 
+        anim.SetBool("run",horizontalInput !=0); // check whether there is a horizontal input or not
+        anim.SetBool("grounded", grounded);
     }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
 }
