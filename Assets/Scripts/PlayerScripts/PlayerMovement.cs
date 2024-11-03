7c775b4c066455c621161a458fa1f43bf302fb93
Vaibhav69Singh <vaibhav9237@gmail.com>
2024-11-03 20:00:35 +0530

    Rolling Completed
    Covered all the problems related to rolling and successfully added rolling functions

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField]private float speed;
     private BoxCollider2D boxCollider;
+    private bool isRolling = false;
     private void Awake()
     {
         //Grab refrences
@@ -33,22 +34,19 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //jump function hai
-        if (Input.GetKey(KeyCode.Space) &&  isGrounded())
+        if (Input.GetKey(KeyCode.Space) &&  IsGrounded())
             Jump();
 
 
         //rolling 
-        if (Input.GetKey(KeyCode.LeftControl) && isGrounded())
+        if (Input.GetKey(KeyCode.LeftControl) && IsGrounded() && !isRolling)
             Roll();
-            
-        
-
             
 
         //set animation 
         anim.SetBool("run",horizontalInput !=0); // check whether there is a horizontal input or not
-        anim.SetBool("grounded", isGrounded());
-
+        anim.SetBool("grounded", IsGrounded());
+        anim.SetBool("rolling", isRolling);
     }
 
     private void Jump()
@@ -58,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    private bool isGrounded()
+    private bool IsGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size,0,Vector2.down,0.1f,groundLayer); 
         return raycastHit.collider != null;
@@ -67,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
     private void Roll()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        isRolling = true;
         if (horizontalInput > 0.01f )
         {
             body.velocity = new Vector2(speed, body.velocity.y);
@@ -78,6 +77,11 @@ public class PlayerMovement : MonoBehaviour
             body.velocity = new Vector2(-speed, body.velocity.y);
             anim.SetTrigger("roll");           
         }
+        Invoke("EndRoll", 0.5f);
         
+    }
+    private void EndRoll()
+    {
+        isRolling = false;
     }
 }
